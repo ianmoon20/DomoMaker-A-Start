2e312fcb54fac5ba0474e227f79fc08f778e5db4
Ian Moon <imm3350@g.rit.edu>
2018-11-03 16:57:13 -0400

    STUFF

--- server/router.js
+++ server/router.js
@@ -4,7 +4,7 @@ const mid = require('./middleware');
 const router = (app) => {
   app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
   app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
-  app.get('/stats', mid.requiresLogin, mid.requiresLogout, controllers.Account.statsPage);
+  app.get('/stats', mid.requiresLogin, controllers.Account.statsPage);
   app.get('/getStats', mid.requiresLogin, controllers.Account.getStats);
   app.get('/getToken', mid.requiresSecure, controllers.Account.getToken);
   app.get('/getDomos', mid.requiresLogin, controllers.Domo.getDomos);
