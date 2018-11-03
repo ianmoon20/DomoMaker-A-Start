c49bcfdf93c33cc164d3f63e62cb22c684aef037
Ian Moon <imm3350@g.rit.edu>
2018-11-03 14:02:12 -0400

    Typooos

--- server/models/Domo.js
+++ server/models/Domo.js
@@ -39,6 +39,7 @@ const DomoSchema = new mongoose.Schema({
 DomoSchema.statics.toAPI = (doc) => ({
   name: doc.name,
   age: doc.age,
+  level: doc.level,
 });
 
 DomoSchema.statics.findByOwner = (ownerId, callback) => {
