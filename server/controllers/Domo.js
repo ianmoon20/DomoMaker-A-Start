83a82a20c6117366234173b9e5dce1c510ae1c03
Ian Moon <imm3350@g.rit.edu>
2018-11-03 19:23:21 -0400

    Fixing update

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -44,12 +44,8 @@ if (req.body.level < 1 || req.body.level > 100) {
     
   Account.update(
     { name: req.session.account.username },
-    {
-        $set: {
-            createdDomos: req.session.account.createdDomos + 1,  
-        }
-    }
-  );
+    { $set: { createdDomos: req.session.account.createdDomos + 1,  }
+  )
 
   return domoPromise;
 };
